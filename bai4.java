3c5d14e3b961a000d5c618e02d48108fe6d230d6
phucphuc1234 <163712885+phucphuc1234@users.noreply.github.com>
2025-02-24 16:44:19 +0700

    Add files via upload

---
+++ bai4.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hinhhoc;
+import java.util.Scanner;
+
+
+class namm {
+    private int nam;
+    public namm(int nam) {
+        this.nam =nam;
+    }
+    public boolean kiemtra() {
+        return (nam % 400 == 0);
+    }
+    public void xuat() {
+        if (kiemtra()) {
+            System.out.println("nam " + nam + " la nam nhuan.");
+        } else {
+            System.out.println("nam " + nam + " not nam nhuan.");
+        }
+    }
+}
+public class bai4 {
+     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap so nam: ");
+        int nam = scanner.nextInt();
+        namm hien=new namm(nam);
+        hien.xuat();
+}
+}
+
+
---
+++ bai6.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hinhhoc;
+import java.util.Scanner;
+class tamgiac {
+  
+    private double canha;
+    private double canhb;
+    private double canhc;
+    public tamgiac (double canha, double canhb, double canhc) {
+        this.canha = canha;
+        this.canhb = canhb;
+        this.canhc = canhc;
+    }
+    public boolean kiemtra() {
+        return (canha+canhb>canhc)&&(canha+canhc>canhb)&&(canhb+canhc>canha);
+    }
+    public void xuat() {
+        if (kiemtra()) {
+            System.out.println("tam giac hop le.");
+        } else {
+            System.out.println("tam giac khong hop le.");
+        }
+    }
+}
+
+public class bai6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap canh a: ");
+        double canha = scanner.nextDouble();
+
+        System.out.print("nhap canh b: ");
+        double canhb = scanner.nextDouble();
+
+        System.out.print("Nhap canh c: ");
+        double canhc = scanner.nextDouble();
+        tamgiac tinh = new tamgiac(canha,canhb,canhc);
+        tinh.xuat();
+    }
+}
+
